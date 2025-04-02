b4866c34d0b747d0da49e91ea0bbdefccbbbcac9
sheraz02 <sheraz.ahmad8814@gmail.com>
2025-04-02 04:52:58 -0400

    Deleted some unused imports and added URLs to Menus.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import { useState } from 'react'
-import Logo from './assets/logo.webp';
-import './App.css'
 import { BrowserRouter as Router, Outlet, Route, Routes, useLocation, Link } from "react-router-dom";
-import DesktopMenu from './components/NavBar/DesktopMenu';
-import { Menus } from './components/constants/utils';
 import Navbar from './components/NavBar/Navbar';
 import Economics from './components/departments/Economics';
-import NavLinks from './components/NavBar/NavLinks';
 import Home from './components/Home/Home';
 import Psychology from './components/departments/Psychology';
 import MassCommunication from './components/departments/MassCommunication';
@@ -44,7 +38,7 @@ const App = () => {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} /> {/* Home Page Route */}
+        <Route index element={<Home />} />
         <Route path="/economics" element={<Economics />} />
         <Route path="/psychology" element={<Psychology />} />
         <Route path="/mass-communication" element={<MassCommunication />} />
--- src/components/constants/Menus.js
+++ src/components/constants/Menus.js
@@ -1,38 +1,38 @@
 export const STUDENTAFFAIRS = [
-    {name: 'Student Portal', link: '#'},
-    {name: 'CPD Activities', link: '#'},
-    {name: 'USP Studio', link: '#'},
-    {name: 'Southern Scholars', link: '#'},
-    {name: 'Athlectics', link: '#'},
-    {name: 'SEDC', link: '#'},
-    {name: 'Sustainability/CSR', link: '#'},
+    {name: 'Student Portal', link: '/portal'},
+    {name: 'CPD Activities', link: '/cpd'},
+    {name: 'USP Studio', link: '/usp-studio'},
+    {name: 'Southern Scholars', link: '/southern-scholars'},
+    {name: 'Athlectics', link: '/athletics'},
+    {name: 'SEDC', link: '/sedc'},
+    {name: 'Sustainability/CSR', link: '/sustainability'},
 ]
 
 
 
 export const POLICIES = [
-    {name: 'Admissions', link: '#'},
-    {name: 'Attendance', link: '#'},
-    {name: 'Fee Refund', link: '#'},
-    {name: 'Late Fee Fine', link: '#'},
-    {name: 'Examination', link: '#'},
-    {name: 'Privacy Policy', link: '#'},
-    {name: 'Employees Grievance', link: '#'},
-    {name: 'MS-M.phil Program', link: '#'},
-    {name: 'Regulations', link: '#'},
+    {name: 'Admissions', link: '/admission-policy'},
+    {name: 'Attendance', link: '/attendance'},
+    {name: 'Fee Refund', link: '/fee-refund'},
+    {name: 'Late Fee Fine', link: '/late-fee-fine'},
+    {name: 'Examination', link: '/examination'},
+    {name: 'Privacy Policy', link: '/privacy-policy'},
+    {name: 'Employees Grievance', link: '/employees-grievance'},
+    {name: 'MS-M.phil Program', link: '/programs'},
+    {name: 'Regulations', link: '/regulations'},
 ]
 
 
 export const SITELINKS = [
-    {name: 'IPE', link: '#'},
-    {name: 'Life at USP', link: '#'},
-    {name: 'Facilities', link: '#'},
-    {name: 'Career', link: '#'},
-    {name: 'Prospectus', link: '#'},
-    {name: 'Admissions', link: '#'},
-    {name: 'Convocation', link: '#'},
-    {name: 'ORIC', link: '#'},
-    {name: 'QEC', link: '#'},
+    {name: 'IPE', link: '/ipe'},
+    {name: 'Life at USP', link: '/life-at-usp'},
+    {name: 'Facilities', link: '/facilities'},
+    {name: 'Career', link: '/career'},
+    {name: 'Prospectus', link: '/prospectus'},
+    {name: 'Admissions', link: '/admissions'},
+    {name: 'Convocation', link: '/convocation'},
+    {name: 'ORIC', link: '/oric'},
+    {name: 'QEC', link: '/qec'},
 ]
 
 export const CONTACT = [
